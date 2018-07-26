1cc788aca292f0b15598d4a8272672e50c220518
Philip Baumann <philip.baumann@ecosia.org>
2018-07-26 11:37:55 +0200

    test

---
+++ server.js
@@ -0,0 +1,46 @@
+// Setup
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+var router = express.Router();
+
+// Routing
+router.get('/', (req, res) => {
+    res.status(200);
+    res.type('text/html');
+    res.send(
+        'Your language is: ' 
+        + req.headers["accept-language"] 
+        + '<br /> You sent a: ' 
+        + req.method 
+        + ' request');
+});
+
+router.post('/', (req, res) => {
+    res.status(200);
+    if (req.query.postVar) {
+        res.type('text/html');
+        res.send('Your POST variable value: ' + req.query.postVar);        
+    } else {
+        res.type('text/html');
+        res.send('Please specify a postVar variable.');
+    }
+});
+
+// Error handling
+router.get((req, res) => {
+   res.status(404);
+   res.end;
+});
+
+router.all((req, res) => {
+    res.status(405);
+    res.end;
+ });
+
+// Register routing
+app.use(router);
+
+// Start the server
+app.listen(port);
+console.log('Magic happens on port ' + port);
