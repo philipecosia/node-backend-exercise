6bde42998b5507d85f52382f2bf5e0f94dddd2b7
Philip Baumann <philip.baumann@ecosia.org>
2018-07-26 16:38:22 +0200

    Proper html wrapping

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.createServer((request, response) => {
     request.on('error', (err) => {
         console.error(err);
         response.statusCode = 400;
-        response.end();
+        response.end('<html><body>Dang sorry!</body></html>');
     });
     
     response.on('error', (err) => {
@@ -21,15 +21,19 @@ http.createServer((request, response) => {
         if (parsed.query.postVar) {
             response.statusCode = 200;
             response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            response.write('<html><body>');
             response.write(
                 'Your POST variable value: '
                 + parsed.query.postVar
             );
+            response.write('</body></html>');
             response.end();
         } else {
             response.statusCode = 400;
             response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            response.write('<html><body>');
             response.write('Please specify a postVar variable.');
+            response.write('</body></html>');
             response.end();
         }
     
@@ -37,23 +41,27 @@ http.createServer((request, response) => {
         
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.write('<html><body>');
         response.write(
             'Your language is: '
             + request.headers['accept-language'] +
             '<br /> You sent a: '
             + request.method
         );
+        response.write('</body></html>');
         response.end();
    
     } else if (request.method !== 'GET' && request.method !== 'POST' ) {
     
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
         response.statusCode = 405;
-        response.end();
+        response.end('<html><body>HTTP method not supported</body></html>');
     
     } else {
    
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
         response.statusCode = 404;
-        response.end();
+        response.end('<html><body>Not found</body></html>');
     
     }
 
